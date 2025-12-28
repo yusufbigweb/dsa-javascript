98a0a36f9b9288b80b19b27b66cb741e30a87369
Yusuf Shaikh <94288456+yusufbigweb@users.noreply.github.com>
2025-12-28 00:08:41 +0000

    only update index file

---
+++ Basic/index.js
@@ -0,0 +1,25 @@
+let a = 10;
+let b = 20;
+let c;
+
+a = a + b;
+b = a - b;
+a = a - b;
+
+//console.log(Math.floor(112/22))
+
+let g = 4567; // task spread number add numbers like: 4+5+6+7
+let h = []
+h.push(g)
+
+//console.log(h.length);
+
+
+let arr = [8,9,0,9]
+
+console.log(indexOf(arr));
+
+//https://youtu.be/h86FQ7vrnds?list=PLbtI3_MArDOmSKABu09sEs0SxCibd1wgr&t=3006
+
+
+
